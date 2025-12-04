6ad771c9ad32eb3256eecc3f5d3a60755d8463ec
Kishlay Kumar <krishraj2408@gmail.com>
2025-12-05 02:14:34 +0530

    more groupby question

--- main.py
+++ main.py
@@ -39,4 +39,39 @@ print(res)
 
 # Save merged clean data into customer_orders_clean.csv
 
-res.to_csv("customer_orders_clean.csv",index=True)
+df_combined.to_csv("customer_orders_clean.csv",index=True)
+
+# highdest_order
+highest_order =df_combined.groupby(['name','customer_id'])['amount'].max().sort_values(ascending=False)
+print(highest_order)
+
+def expensive(amount):
+    if amount >5000:
+        return "Expensive"
+    else:
+        return "Normal"
+    
+df_combined['expensive']=df_combined['amount'].apply(expensive)
+print("added column")
+print(df_combined)
+
+total_revenue=df_combined.groupby('category').agg({
+    'amount':'sum'
+})
+print(total_revenue)
+
+pivot_table = pd.pivot_table(
+    df_combined,
+    values="amount",
+    index="city",
+    columns="category",
+    aggfunc="sum",
+    fill_value=0       # replaces NaN with 0
+)
+
+print(pivot_table)
+
+
+
+df_combined.to_csv("customer_orders_clean.csv",index=False)
+    
